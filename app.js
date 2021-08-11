7963698c6610c7e54b38618520751f5b147d97a6
andjmorrison <ajmorrison@ucdavis.edu>
2021-08-11 16:16:01 -0700

    fix regex split for system identifier

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ javascript: (() => {
 
     Object.keys(ids).map(function(e, i) {
         let dataId = ids[i].getAttribute('data-recordID');
+        console.log('dataId: ', dataId)
         let dataTitle = titles[i].firstElementChild.innerText;
 
-        let system = dataId.match(/[a-zA-Z]/g).join('');
+        let system = dataId.match(/[a-zA-Z]?_*/g).join('').replaceAll('_', ' ').toUpperCase();
+        console.log('system: ', system)
         let id = dataId.match(/[0-9]/g).join('');
         
-        let listItemArray = document.getElementsByClassName('list-item');
-
+        let listItemArray = document.getElementsByClassName('result-item-details');
         
         let systemNode = document.createElement('H3');
         systemNode.setAttribute('style','color:red');
@@ -19,7 +20,6 @@ javascript: (() => {
         systemNode.appendChild(systemTextNode);
         listItemArray[i].appendChild(systemNode);
 
-        
         let idNode = document.createElement('H3');
         idNode.setAttribute('style', 'color:red');
         idNode.style.textAlign = 'center';
